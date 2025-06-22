8f7eb327863fd608fbf0e88c6d3ffac6482074f2
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-06-22 01:21:00 +0100

    Tweaked the original deposit and withdraw function working perfectly now, tessted n remix

--- src/SincWrapper.sol
+++ src/SincWrapper.sol
@@ -8,13 +8,34 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract SincWrapper is ERC4626{
 
 
-    IERC20 asset;
     
-    constructor(IERC20 _asset) ERC20("Sinc Vault", "SVT") ERC4626(_asset) {
-        asset = _asset;
+    uint256 exchangeRate;
+    
+    constructor(IERC20 _asset, uint256 _exchangeRate) ERC20("Sinc Vault", "SVT") ERC4626(_asset) {
+        
+        exchangeRate = _exchangeRate;
     }
 
+    function calcDeposit(uint256 _amount) public view returns (uint256) {
+        uint256 amount = _amount * exchangeRate;
+        return amount;
+    }
 
+    function calcWithdraw (uint256 _amount) public view returns (uint256){
+        uint256 amount = _amount / exchangeRate;
+        return amount;
+    }
 
+    function deposit(uint256 assets, address receiver) public override returns (uint256) {
+
+       return super.deposit(calcDeposit(assets), receiver);
+
+        
+    }
+
+    function withdraw(uint256 assets, address receiver, address owner) public override returns (uint256) {
+        return super.withdraw(calcWithdraw(assets), receiver, owner);
+        
+    }
 
 }
