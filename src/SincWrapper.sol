6f740f5e1f5dcc6f247d0f0b295f9a3ca9023db4
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-06-22 01:31:34 +0100

    Later found out you can manipulate by just chaning the convert to asset or convert to shares function

--- src/SincWrapper.sol
+++ src/SincWrapper.sol
@@ -2,31 +2,25 @@
 pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SincWrapper is ERC4626 {
-    uint256 exchangeRate;
+    uint256 public immutable exchangeRate;
 
-    constructor(IERC20 _asset, uint256 _exchangeRate) ERC20("Sinc Vault", "SVT") ERC4626(_asset) {
+    constructor(IERC20 _asset, uint256 _exchangeRate)
+        ERC20("Sinc Vault", "SVT")
+        ERC4626(_asset)
+    {
+        require(_exchangeRate > 0, "Invalid exchange rate");
         exchangeRate = _exchangeRate;
     }
 
-    function calcDeposit(uint256 _amount) public view returns (uint256) {
-        uint256 amount = _amount * exchangeRate;
-        return amount;
+    function convertToShares(uint256 assets) public view override returns (uint256) {
+        return assets * exchangeRate;
     }
 
-    function calcWithdraw(uint256 _amount) public view returns (uint256) {
-        uint256 amount = _amount / exchangeRate;
-        return amount;
-    }
-
-    function deposit(uint256 assets, address receiver) public override returns (uint256) {
-        return super.deposit(calcDeposit(assets), receiver);
-    }
-
-    function withdraw(uint256 assets, address receiver, address owner) public override returns (uint256) {
-        return super.withdraw(calcWithdraw(assets), receiver, owner);
+    function convertToAssets(uint256 shares) public view override returns (uint256) {
+        return shares / exchangeRate;
     }
 }
