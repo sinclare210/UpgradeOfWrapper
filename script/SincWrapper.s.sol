9ad192cff07029337c8d03ce2b7005fc6e9c31c9
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-06-22 06:07:05 +0100

    Added natspec for easy understanding of the code base by a reader

--- script/SincWrapper.s.sol
+++ script/SincWrapper.s.sol
@@ -2,16 +2,17 @@
 pragma solidity ^0.8.27;
 
 import {Script, console} from "forge-std/Script.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {SincWrapper} from "../src/SincWrapper.sol";
-//https://sepolia.etherscan.io/address/0xe3094bfa319e84182f757d9fcacf49e53ef223de#writeContract
+//https://sepolia.etherscan.io/address/0x86c6d274517c13d8c6649764b3b8de5d7a08114b
 
 contract SincWrapperScript is Script {
     SincWrapper public sincWrapper;
 
     function run() public {
         vm.startBroadcast();
-        sincWrapper = new SincWrapper(0x75fBb602367D3e5d798b5e2152C6efa7A48a015b, 500000);
+        sincWrapper = new SincWrapper(IERC20(0x75fBb602367D3e5d798b5e2152C6efa7A48a015b), 500000);
         vm.stopBroadcast();
     }
 }
--- src/SincWrapper.sol
+++ src/SincWrapper.sol
@@ -5,21 +5,45 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/**
+ * @title SincWrapper Vault
+ * @author
+ * @notice A fixed-rate ERC4626-compliant vault for wrapping ERC20 tokens
+ * @dev This vault overrides convertToShares and convertToAssets using a fixed exchange rate
+ */
 contract SincWrapper is ERC4626 {
+    /**
+     * @notice Fixed exchange rate between assets and shares (e.g., 1 asset = X shares)
+     */
     uint256 public immutable exchangeRate;
 
-    constructor(IERC20 _asset, uint256 _exchangeRate)
-        ERC20("Sinc Vault", "SVT")
-        ERC4626(_asset)
-    {
+    /**
+     * @notice Constructs the SincWrapper vault
+     * @dev Inherits from OpenZeppelin ERC4626 and ERC20
+     * @param _asset The underlying ERC20 token the vault accepts
+     * @param _exchangeRate The multiplier used to convert assets into shares
+     */
+    constructor(IERC20 _asset, uint256 _exchangeRate) ERC20("Sinc Vault", "SVT") ERC4626(_asset) {
         require(_exchangeRate > 0, "Invalid exchange rate");
         exchangeRate = _exchangeRate;
     }
 
+    /**
+     * @notice Converts a given amount of underlying assets to vault shares
+     * @dev Uses a fixed exchangeRate: shares = assets * exchangeRate
+     * @param assets The amount of underlying tokens to convert
+     * @return The number of shares corresponding to the input assets
+     */
     function convertToShares(uint256 assets) public view override returns (uint256) {
         return assets * exchangeRate;
     }
 
+    /**
+     * @notice Converts a given amount of vault shares back to underlying assets
+     * @dev Uses a fixed exchangeRate: assets = shares / exchangeRate
+     * @param shares The number of shares to convert
+     * @return The amount of underlying tokens corresponding to the shares
+     */
     function convertToAssets(uint256 shares) public view override returns (uint256) {
         return shares / exchangeRate;
     }
